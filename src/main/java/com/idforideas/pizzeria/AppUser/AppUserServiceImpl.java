bba9f5a9a89c253f8a67bd73616ce3d439d5039f
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-03-01 23:07:35 -0400

    added SecurityConfig

--- src/main/java/com/idforideas/pizzeria/AppUser/AppUserServiceImpl.java
+++ src/main/java/com/idforideas/pizzeria/AppUser/AppUserServiceImpl.java
@@ -1,7 +1,13 @@
 package com.idforideas.pizzeria.AppUser;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +20,23 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2/28/2022
  */
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
     private final AppUserRepo userRepo;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser user = this.userRepo.findByEmail(email);
+        if(user == null) {
+            String msg = "Email not found in the database";
+            log.error(msg);
+            throw new UsernameNotFoundException(msg);
+        } else {
+            log.info("Email found in the database: {}", email);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser appUser) {
@@ -33,6 +54,5 @@ public class AppUserServiceImpl implements AppUserService {
     public List<AppUser> getUsers() {
         log.info("Ftching all users");
         return this.userRepo.findAll();
-    }
-    
+    } 
 }
