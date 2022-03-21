b66d0f5e9649622859d2a04d9aa4873332025cc5
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-03-21 15:55:06 -0400

    fix dependency and added ApiInfo

---
+++ src/main/java/com/idforideas/pizzeria/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.idforideas.pizzeria.config;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.idforideas.pizzeria"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("API de la pizzeria Don Remolo",
+            "API para la solicitud de pedidos de productos a la pizzeria",
+            "1.0",
+            "",
+            new Contact("Equipo # 221 de ID FOR IDEAS", "www.idforideas.org", "pizzeriadonremolo@gmail.com"),
+            "MIT",
+            "url.licence",
+            new ArrayList<>());
+    }
+    
+}
