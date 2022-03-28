0621c862e801b0cfa0283747b545a43fd4772563
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-03-28 10:46:38 -0400

    fix name file AppUserResource

--- src/main/java/com/idforideas/pizzeria/appuser/AppUserResource.java
+++ src/main/java/com/idforideas/pizzeria/appuser/AppUserResource.java
@@ -22,8 +22,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.idforideas.pizzeria.utils.Response;
 
