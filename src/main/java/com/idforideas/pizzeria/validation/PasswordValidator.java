8a31e7426412e06fd962e41c9c35d386639c7609
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-03-29 12:18:01 -0400

    fix regexp in PasswordValidator

--- src/main/java/com/idforideas/pizzeria/validation/PasswordValidator.java
+++ src/main/java/com/idforideas/pizzeria/validation/PasswordValidator.java
@@ -13,7 +13,7 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
         log.info("This is pwd {}", pwd);
         if( pwd == null || pwd.isEmpty() || pwd.isBlank()){ log.info("Pwd is blank"); return false;}
         if( pwd.length() < 8 ) { log.info("pwd.length() = {}",pwd.length()); return false;}
-        if( !pwd.matches("[0-9]") ) { log.info("pwd not [0-9]"); return false;}
+        if( !pwd.matches("+[0-9]") ) { log.info("pwd not [0-9]"); return false;}
         if( !pwd.matches("[a-z]") ) { log.info("pwd not [a-z]"); return false;}
         if( !pwd.matches("[A-Z]") ) { log.info("pwd not [A-Z]"); return false;}
         if( !pwd.matches("[@#$%^&+=]") ) { log.info("pwd not special character"); return false;}
