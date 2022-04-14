517f717f44ef3f8c2751e517cacd6a305192cbcd
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-04-14 10:23:44 -0400

    refactoring PasswordValidator

--- src/main/java/com/idforideas/pizzeria/validation/PasswordValidator.java
+++ src/main/java/com/idforideas/pizzeria/validation/PasswordValidator.java
@@ -1,42 +1,37 @@
 package com.idforideas.pizzeria.validation;
 
+import java.util.function.BiPredicate;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
 
 public class PasswordValidator implements ConstraintValidator<Password, String> {
 	private static final int MIN_LENGTH = 8;
 	private static final String ESPECIAL_CHARACTERS = "@#$%^&+=";
 
+	private static final IntPredicate IS_SPACE_CHAR = Character::isSpaceChar;
+	private static final IntPredicate IS_DIGIT = Character::isDigit;
+	private static final IntPredicate IS_UPPERCASE = Character::isUpperCase;
+	private static final IntPredicate IS_LOWERCASE = Character::isLowerCase;
+	private static final BiPredicate<IntStream, IntStream> CONTAINS = (pwd, match) -> pwd.anyMatch(p -> match.anyMatch(m -> p==m));
+
     @Override
     public boolean isValid(String arg0, ConstraintValidatorContext arg1) {
         if(arg0 == null || arg0.isEmpty() || arg0.isBlank()) return false;
         if(arg0.length() < MIN_LENGTH) return false;
-		if(containsEspace(arg0)) return false;
-        if(!containsDigits(arg0)) return false;
-        if(!containsLowerCase(arg0)) return false;
-        if(!containsUpperCase(arg0)) return false;
-        if(!containsEspecialCharacter(arg0)) return false;
+		if(anyMatch(arg0, IS_SPACE_CHAR)) return false;
+        if(!anyMatch(arg0, IS_DIGIT)) return false;
+        if(!anyMatch(arg0, IS_UPPERCASE)) return false;
+        if(!anyMatch(arg0, IS_LOWERCASE)) return false;
+        if(!CONTAINS.test(arg0.chars(), ESPECIAL_CHARACTERS.chars())) return false;
 
 		return true;
     }
-    
-    private static boolean containsEspace(String arg) {
-		return arg.chars().anyMatch(Character::isSpaceChar);
-	}
 
-	private static boolean containsDigits(String arg) {
-		return arg.chars().anyMatch(Character::isDigit);
-	}
-
-	private static boolean containsUpperCase(String arg) {
-		return arg.chars().anyMatch(Character::isUpperCase);
-	}
-
-	private static boolean containsLowerCase(String arg) {
-		return arg.chars().anyMatch(Character::isLowerCase);
-	}
-
-	private static boolean containsEspecialCharacter(String arg) {
-		return arg.chars().anyMatch(a -> ESPECIAL_CHARACTERS.chars().anyMatch(b -> a==b));
+	private static boolean anyMatch(String pwd, IntPredicate match) {
+		return pwd.chars().anyMatch(match);
 	}
 }
