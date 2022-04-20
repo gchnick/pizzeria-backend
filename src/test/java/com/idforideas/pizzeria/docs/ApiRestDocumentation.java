b48f40bda2d1d86c33eac25db7c623003c5220d4
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-04-20 19:13:36 -0400

    init spring auto rest docs

--- src/test/java/com/idforideas/pizzeria/docs/ApiRestDocumentation.java
+++ src/test/java/com/idforideas/pizzeria/docs/ApiRestDocumentation.java
@@ -1,128 +1,51 @@
 package com.idforideas.pizzeria.docs;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.List;
-import java.util.ListIterator;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.idforideas.pizzeria.category.Category;
-import com.idforideas.pizzeria.category.CategoryService;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
 public class ApiRestDocumentation {
-        
-    MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-            .apply(documentationConfiguration(restDocumentation)
-                .operationPreprocessors()
-                    .withRequestDefaults(prettyPrint())
-                    .withResponseDefaults(prettyPrint())).build();
-    }
-
-    @Tag("Category")
-    @Nested
-    class categoryResource {
-        final String URL_TEMPLATE = "/api/v1/categories";
-        final int TOTAL_ELEMENTS = 4;
-        Page<Category> categories;
-        Pageable pageable;
-
-        @MockBean
-        CategoryService categoryService;
-
-        @BeforeEach
-        void setUp() {
-            List<Category> categories = Stream.of(
-                new Category("Pizzas"),
-                new Category("Empanadas"),
-                new Category("Bebidas"),
-                new Category("Postres"))
-                .collect(Collectors.toList());
     
-            ListIterator<Category> iterator = categories.listIterator();
-            while(iterator.hasNext()) {
-                Long id = Long.valueOf( Integer.valueOf(iterator.nextIndex()).longValue() );
-                iterator.next().setId(id);
-            }
-            
-            this.pageable = PageRequest.of(0, 2, Sort.by("name").descending());
-            this.categories = new PageImpl<>(categories, pageable, categories.size());
-        }
+    @Autowired
+    private WebApplicationContext context;
 
-        @Tag("GET")
-        @Test
-        void getList() throws Exception {
-            final String VARIABLES = "?page=0&size=2&sort=name,desc";
+    @Autowired
+    protected ObjectMapper objectMapper;
 
-            // Given
-            when(categoryService.list(pageable)).thenReturn(categories);
+    protected mockMvc mockMvc;
 
-            // When
-            mockMvc.perform(get(URL_TEMPLATE + VARIABLES).contentType(APPLICATION_JSON))
-            // Then
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON))
-                .andExpect(jsonPath("$.data.categories").exists())
-                .andExpect(jsonPath("$.data.categories.numberOfElements").value(2))
-                .andExpect(jsonPath("$.data.categories.totalElements").value(TOTAL_ELEMENTS))
-                .andDo(document("{class-name}/{method-name}",requestParameters(
-                        parameterWithName("page")
-                        .description("Numero de pagina a solicitar. Por defecto su valor es 0 (primera pagina)"),
-                        parameterWithName("size")
-                        .description("Cantidad de categorias por pagina. Por defecto su valor es 5"),
-                        parameterWithName("sort")
-                        .description("Nombre del campo que se usara para ordenar la lista. Por defecto se ordena la lista por nombre de categoria de manera ascendente")),
-                        relaxedResponseFields(
-                            fieldWithPath("data.categories.totalPages")
-                            .description("Cantidad total de paginas disponibles"),
-                            fieldWithPath("data.categories.totalElements")
-                            .description("Total de categorias registradas"),
-                            fieldWithPath("data.categories.pageable.pageNumber")
-                            .description("Pagina de categorias actual"),
-                            fieldWithPath("data.categories.numberOfElements")
-                            .description("Cantidad de categorias en la pagina actual"),
-                            fieldWithPath("data.categories.content")
-                            .description("Lista de categorias de la pagina actual")
-                        )
-                ));
-        }
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilters(springSecurityFilterChain)
+                .alwaysDo(JacksonResultHandlers.prepareJackson(objectMapper))
+                .alwaysDo(MockMvcRestDocumentation.document("{class-name}/{method-name}",
+                        Preprocessors.preprocessRequest(),
+                        Preprocessors.preprocessResponse(
+                                ResponseModifyingPreprocessors.replaceBinaryContent(),
+                                ResponseModifyingPreprocessors.limitJsonArrayLength(objectMapper),
+                                Preprocessors.prettyPrint())))
+                .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation)
+                        .uris()
+                        .withScheme("https")
+                        .withHost("heroku.app.pizzariadonremolo")
+                        .withPort(8080)
+                        .and().snippets()
+                        .withDefaults(CliDocumentation.curlRequest(),
+                                HttpDocumentation.httpRequest(),
+                                HttpDocumentation.httpResponse(),
+                                AutoDocumentation.requestFields(),
+                                AutoDocumentation.responseFields(),
+                                AutoDocumentation.pathParameters(),
+                                AutoDocumentation.requestParameters(),
+                                AutoDocumentation.description(),
+                                AutoDocumentation.methodAndPath(),
+                                AutoDocumentation.section()))
+                .build();
     }
 }
