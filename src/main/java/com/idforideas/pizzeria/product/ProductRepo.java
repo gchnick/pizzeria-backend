152defd9c0e9ee9c47ffb50057a700ded5405155
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-04-21 18:14:09 -0400

    added test integration of ProductRepo

--- src/main/java/com/idforideas/pizzeria/product/ProductRepo.java
+++ src/main/java/com/idforideas/pizzeria/product/ProductRepo.java
@@ -1,6 +1,6 @@
 package com.idforideas.pizzeria.product;
 
-import java.util.Collection;
+import java.util.List;
 
 import com.idforideas.pizzeria.category.Category;
 
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
-    Collection<Product> findByCategory(Category category);
+    List<Product> findByCategory(Category category);
     
     Page<Product> findByCategory(Category category, Pageable pageable);
 }
---
+++ src/test/java/com/idforideas/pizzeria/product/ProductRepoIT.java
@@ -0,0 +1,38 @@
+package com.idforideas.pizzeria.product;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+
+import java.util.List;
+
+import com.idforideas.pizzeria.category.Category;
+import com.idforideas.pizzeria.category.CategoryRepo;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class ProductRepoIT {
+
+    @Autowired
+    ProductRepo productRepo;
+
+    @Autowired
+    CategoryRepo categoryRepo;
+
+    @Test
+    void return_list_of_product_by_category_ignore_case() {
+
+        Category pizzasIgnoreCase = categoryRepo.findByNameIgnoreCase("pIzZas").get();
+
+        List<Product> actual = productRepo.findByCategory(pizzasIgnoreCase);
+
+        assertEquals("Pizzas", pizzasIgnoreCase.getName());
+        assertFalse(actual.isEmpty());
+        assertFalse(actual.stream().anyMatch(p -> !"pizzas".equalsIgnoreCase(p.getCategory().getName())));
+    }
+}
